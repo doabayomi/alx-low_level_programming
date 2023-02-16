f5a551da13fdf8d11434410a93de5957175d6cfb
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-16 15:15:14 -0800

    Fight go soon sup

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - the main function of the program
+ * @str[]: Defined string
+ *
+ * Description: Prints out the defined string in the standard
+ * output
+ * Return: zero value
+ */
+int main(void)
+{
+	/*The string is stored in the str variable. */
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main (){
+/**
+ * main - The everyday main of C
+ *
+ * Description: A program that prints out a string to the standard output
+ * using the printf() function
+ * Return: zero value
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }

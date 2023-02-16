97945261738a3c3ebb8fdb1bcc56b9d2841fca67
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-16 15:38:02 -0800

    Lets see, shall we

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - The standard main function of C
+ * @intvar: Integer data type
+ * @charvar: Character data type
+ * @floatvar: Floating variable data type
+ * @longvar: Long integer data type
+ * @doublelongvar: Long long integer data type
+ *
+ * Description: This is a program that prints the sizes of various data types
+ * in C measured in bytes. This value will vary based on operating system.
+ * Return: 0
+ */
+int main(void)
+{
+	char charvar;
+	int intvar;
+	long int longvar;
+	long long int doublelongvar;
+	float floatvar;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(charvar));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intvar));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longvar));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(doublelongvar));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatvar));
+	
+	return (0);
+}

6c6ee79051e643ff30b7d825a087f7a10a11d1ff
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-16 16:13:51 -0800

    Ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,18 @@
 
 /**
  * main - the main function of C
+ * @str[]: String to be sent to output terminal
+ *
  * Description: A program that prints a string without printf() or
  * puts() followed by a new line, to standard error.
  * Return: 1 always
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
+	fflush(stdout);
+	
 	return (1);
 }

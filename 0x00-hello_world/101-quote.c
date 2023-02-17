1c142330d2e7f835fd734cca0b473d452da7fa96
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-16 16:14:58 -0800

    Perfectly balanced as all things should be

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,6 @@ int main(void)
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
 	fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
-	fflush(stdout);
-	
+	fflush(stdout);	
 	return (1);
 }

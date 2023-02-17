58741ae54c04b2f8938c1d0d664f66e581938999
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-16 16:08:55 -0800

    Finishing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints "and that piece of art is useful" - - Dora Korpar
- * followed by a new line, to standard error.
+ * main - the main function of C
+ * Description: A program that prints a string without printf() or
+ * puts() followed by a new line, to standard error.
  * Return: 1 always
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

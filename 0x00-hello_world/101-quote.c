c0b530f02795d849d196adf1c5cb359e67cd9ae8
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-16 16:17:07 -0800

    Lets get this shit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,6 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
 	fwrite(str, sizeof(char), sizeof(str) - 1, stdout);
 	fflush(stdout);	
 	return (1);

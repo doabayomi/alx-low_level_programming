aff9c7bc38d553a546604ed80f26b7b03f1fb400
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 11:08:48 -0800

    I'm afrsid

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,12 @@
 int print_last_digit(int num)
 {
 	/* The last digit is the remainder of the number when divided by 10 */
-	int last_digit = abs(num) % 10;
+	int last_digit = num % 10;
+	
+	if (last_digit < 0)
+	{
+		last_digit = abs(last_digit);
+	}
 	_putchar(last_digit + '0');
 	return (last_digit);
 }

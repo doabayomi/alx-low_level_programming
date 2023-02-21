add5c7c07682a6f671c705a961b3c79be879a37b
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 14:43:45 -0800

    Shooting shots

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+	unsigned long int previous = 0, sum = 0, next = 1, current;
+
+	while (1)
+	{
+		current = previous + next;
+		if (current <= 4000000)
+		{
+			if (current % 2 == 0)
+				sum = sum + current;
+			previous = next;
+			next = current;
+		}
+		else
+		{
+			break;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}

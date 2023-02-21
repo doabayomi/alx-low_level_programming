166ab701c9bb7ab64eff2b942f7e3d717de6ad4e
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 12:28:07 -0800

    Timing

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * times_table - Program to print the nine times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	/**
+	 * The idea is to use two loop to print each number
+	 *  and multiply it by the second number and then a new line
+	 */
+	int multiplier, multiplied;
+
+	for (multiplier = 0; multiplier <= 9; multiplier++)
+	{
+		for (multiplied = 0; multiplied <= 9; multiplied++)
+		{
+			printf("%d", (multiplier * multiplied));
+			if (multiplied < 9)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}

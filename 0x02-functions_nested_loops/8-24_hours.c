5dd3dae30a1b2864c323f45e8590a69591ba6bff
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 12:12:26 -0800

    Longest Day of My Life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,11 +14,11 @@ void jack_bauer(void)
 	int hours, minutes;
 
 	for (hours = 0; hours < 24; hours++)
-	{		
+	{
 		for (minutes = 0; minutes < 60; minutes++)
 		{
 			printf("%02d:%02d", hours, minutes);
+			printf("\n");
 		}
-		printf("\n");
 	}
 }

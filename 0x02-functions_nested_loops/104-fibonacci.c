b17eb12210889ecd0c4b14290d8ee37e57185f27
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 15:06:25 -0800

    Whatever happens

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,14 +10,14 @@ int main(void)
 	int i = 0;
 	unsigned long int previous = 0, next = 1, current;
 
-	while (i < 93)
+	while (i < 98)
 	{
 		current = previous + next;
 		printf("%lu", current);
 		previous = next;
 		next = current;
 		/* Dont ask me how I got this number, I am just playing */
-		if (i != 92)
+		if (i != 97)
 			printf(", ");
 		i++;
 	}

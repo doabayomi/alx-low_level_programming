7c3c6f81d3b04819fa4132c597bcc27de20e8234
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 03:36:12 -0800

    Betty, Oh Betty

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,8 @@
  *
  * Return: The last digit of the integer (num % 10)
  */
-int int print_last_digit(int num)
+int print_last_digit(int num)
 {
 	/* The last digit is the remainder of the number when divided by 10 */
-	return(num % 10);
+	return (num % 10);
 }

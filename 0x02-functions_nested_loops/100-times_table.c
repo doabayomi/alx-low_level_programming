4c5ab39bd1fb4b393592e64fe0c7849b1f5de208
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 13:58:27 -0800

    To be or not to be

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,7 @@ void print_times_table(int n)
 	 */
 	int multiplier, multiplied;
 
-	if (n < 0 || n > 15)
-	{
-		printf("");
-	}
-	else
+	if (n > 0 || n < 15)
 	{
 		for (multiplier = 0; multiplier <= n; multiplier++)
 		{

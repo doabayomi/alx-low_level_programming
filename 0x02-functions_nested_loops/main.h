592187352cf1bfc4c0b1323ffaa919c7713a2a99
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-21 03:17:32 -0800

    Adding important scripts

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * This is the header file that includes all the 
+ * function prototypes in this ALX project.
+ */
+
+int  _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+

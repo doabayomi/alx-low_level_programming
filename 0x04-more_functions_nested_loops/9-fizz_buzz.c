9b42dc9646e59c17a1de0a9ca07f4893387ad39b
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-23 13:15:42 -0800

    Looking again

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,12 +14,14 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		printf("%d ", i);
 		if (i % 3 == 0)
-			printf("Fizz");
-		if (i % 5 == 0)
-			printf("Buzz");
-		printf(" ");
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 5 == 0) && (i % 3 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);

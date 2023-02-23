a70593b83aa924b3d5388dca05c3c790bb7fb037
Daniel Abayomi <dan234bayomi@gmail.com>
2023-02-23 06:50:43 -0800

    Laking

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,7 @@ void more_numbers(void)
 {
 	int i, j;
 	char n[] = "01234567891011121314";
-	int n_length = sizeof(n) / sizeof(n[0]);
-
+	
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)

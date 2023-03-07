fb8d37a25ff627f5bc8f979f1bb43cb4f5d28d5a
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-06 18:51:08 -0800

    Righting my wrongs

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,27 +12,25 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
+	unsigned int i = 0, j, matches = 0;
 	unsigned int match_set = strlen(accept);
-	unsigned int i = 0, j = 0, match = 0, next = 0;
 
-	while (s[i] != '\0')
+	for (; s[i] != '\0'; i++)
 	{
-		/* Confirming if the current character is a match */
-		for (; j < match_set; j++)
+		int next = 0;
+
+		for (j = 0; j < match_set; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				match++;
+				matches++;
 				break;
 			}
 		}
-		/* Breaking if first instance fails to match */
-		if (i == 0 && match == 0)
+
+		if (i == 0 && matches == 0)
 			break;
-		/**
-		 * Confirming next instance match to determine course of
-		 * action (continuing or breaking loop)
-		*/
+
 		for (j = 0; j < match_set; j++)
 		{
 			if (s[i + 1] == accept[j])
@@ -41,10 +39,11 @@ unsigned int _strspn(char *s, char *accept)
 				break;
 			}
 		}
+
 		if (next == 1)
-			i++;
+			continue;
 		else
 			break;
 	}
-	return (match);
+	return (matches);
 }

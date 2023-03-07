7a838779c828a80b1eac6c8058d59a203777ffa9
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-06 19:28:17 -0800

    Pompei

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ char *_strpbrk(char *s, char *accept);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
+void set_string(char **s, char *to);
 #endif

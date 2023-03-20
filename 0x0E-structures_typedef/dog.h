050809515cbea524a868b524bb536ca7b609b182
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-20 08:03:22 -0700

    Betty documentation added to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
-#if !defined(DOG_H_)
+#ifndef DOG_H_
 #define DOG_H_
 
+/**
+ * struct dog - Simple structure of a dog.
+ * @name: Name of dog.
+ * @age: Age of dog to the decimal.
+ * @owner: Name of dog's owner.
+ */
 struct dog
 {
 	char *name;

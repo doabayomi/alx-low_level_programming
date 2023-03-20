a7cf38062e9df2e718d3e38e74709813546ab2dc
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-20 12:29:00 -0700

    Fixing return

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,8 +12,9 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog = malloc(sizeof(dog_t));
+
 	if (n_dog == NULL)
-		return NULL;
+		return (NULL);
 
 	n_dog->name = name;
 	n_dog->age = age;

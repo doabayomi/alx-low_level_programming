91869de3bf3347ddd82facb843068d2e463e9af3
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-20 18:30:10 -0700

    Floats

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog = malloc(sizeof(dog_t));
 
-	if (name == NULL || !age || owner == NULL)
+	if (name == NULL || !(age < 0) || owner == NULL)
 		return (NULL);
 
 	if (n_dog == NULL)

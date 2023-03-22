28f4fce75142eb3ecfb423f20b3ca5c6f38c523a
Daniel Abayomi <dan234bayomi@gmail.com>
2023-03-22 03:19:36 -0700

    Handled iteration

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - performs functions on all elements of an input array.
+ * @array: Input array
+ * @size: Size of input array
+ * @action: function to implement on members of array
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	for (; i < size; i++)
+		(*action)(array[i]);
+}

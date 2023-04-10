95a0d98e3d1a17fa2cf8f98450b36afe98f3b292
Daniel Abayomi <dan234bayomi@gmail.com>
2023-04-10 13:53:39 -0700

    Fixed second task

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,23 +9,8 @@
 */
 void print_binary(unsigned long int n)
 {
-	unsigned long int i;
-	int one_hit = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	for (i = 1 << 31; i > 0; i = i / 2)
-	{
-		if (n & i)
-		{
-			_putchar('1');
-			one_hit = 1;
-		}
-		else
-		{
-			if (one_hit)
-				_putchar('0');
-		}
-
-		if (one_hit == 0 && i == 1)
-			_putchar('0');
-	}
+	_putchar((n & 1) + '0');
 }

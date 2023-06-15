2b2ff65bf16f20c34d9e78f2aa2f369094ac10ce
dan1bayomi <dan234bayomi@gmail.com>
2023-06-15 08:07:25 -0700

    Fixing insert bug. Null case

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,9 +14,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node;
 	unsigned int i = 0;
 
-	if ((*h) == NULL || h == NULL)
-		return (NULL);
-
 	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -24,11 +21,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new_node->n = n;
-		new_node->next = prev_node;
-		(new_node->next)->prev = new_node;
+		new_node->next = *h;
+		if ((*h) != NULL)
+			(*h)->prev = new_node;
 		new_node->prev = NULL;
 		(*h) = new_node;
-
 		return (new_node);
 	}
 

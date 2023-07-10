18180902ed38d3e5fee742eef9cbc062bd6b7401
dan1bayomi <dan234bayomi@gmail.com>
2023-07-10 15:59:54 -0700

    Working on create tables:

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,8 +12,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i = 0;
 
 	hash_table = (hash_table_t *)malloc(sizeof(hash_table));
+	if (hash_table == NULL)
+		return (NULL);
 	hash_table->size = size;
 	hash_table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+		return (NULL);
 
 	for (; i < hash_table->size; i++)
 	{
